e895eea84b82dd397caea501660f03eccecf8c9f
Jordan Gillard <jordan-gillard@outlook.com>
2021-04-30 19:46:40 -0500

    Jupyter Notebook now able to find dissimilar functions between versions

--- sample_files/funcs_v1.py
+++ sample_files/funcs_v1.py
@@ -1,3 +1,11 @@
 def assign_numbers():
     x = 1
     y = 2 + 3
+
+
+def func_with_params(a, b, c):
+    pass
+
+
+def same_func(a):
+    return 1
--- sample_files/funcs_v2.py
+++ sample_files/funcs_v2.py
@@ -2,3 +2,10 @@ def assign_numbers():
     x = 1
     y = 2 + 3
     z = 4 + 5 + 6
+
+
+def func_with_params(a, b, c):
+    return 1
+
+def same_func(a):
+    return 1
