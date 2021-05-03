84e8c5ccbcc0f3a3ec81dddfb4cc7d14c752f7b5
Jordan Gillard <jordan-gillard@outlook.com>
2021-05-02 20:53:14 -0500

    feat: Implements GitHandler class
    This commit implements the ability to change between commits
    and reset files to previous states. Still needs tests.

--- versionizer/git_handler.py
+++ versionizer/git_handler.py
@@ -1,15 +1,28 @@
-from git import repo
+from typing import Optional
+
+from git import Commit, Repo
 
 
 class GitHandler:
-    def __init__(self, first_commit, second_commit, file):
-        self.first_commit = first_commit
-        self.second_commit = second_commit
+    def __init__(self, file: str, first_commit: str, second_commit: Optional[str] = None):
+        self.repo = Repo(search_parent_directories=True)
+        self.master: Commit = self.repo.head.reference
+        self.first_commit: Commit = self.repo.commit(first_commit)
+        if second_commit:
+            self.second_commit: Commit = self.repo.commit(second_commit)
+        else:
+            self.second_commit = self.master
         self.file = file
 
+    def _checkout_commit(self, commit: Commit):
+        self.repo.head.reference = commit
+        self.repo.head.reset(index=True, working_tree=True)
+
     def checkout_first_commit(self):
-        pass
-        # past_branch = repo.create_head('commit_1', 'HEAD~10')
+        self._checkout_commit(self.first_commit)
+
+    def checkout_second_commit(self):
+        self._checkout_commit(self.second_commit)
 
     def get_ast_for_first_commit(self):
         pass
@@ -18,4 +31,5 @@ class GitHandler:
         pass
 
     def return_to_head(self):
-        pass
+        self.repo.head.reference = self.master
+        self.repo.head.reset(index=True, working_tree=True)
