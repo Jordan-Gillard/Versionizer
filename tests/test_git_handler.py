c2947d9d0665b3246d688a1973099e1d6a711dcd
Jordan Gillard <jordan-gillard@outlook.com>
2021-05-11 19:51:33 -0500

    tests: Fixes test_git_handler so that it stashes changes

--- tests/test_git_handler.py
+++ tests/test_git_handler.py
@@ -22,16 +22,20 @@ def dummy_file_name():
 
 def test_dummy_file_is_destroyed_when_going_to_previous_commit(dummy_file_name):
     git_handler = GitHandler('HEAD~3')
+    git_handler.stash_changes_if_necessary()
     git_handler.checkout_first_commit()
     exists = os.path.exists(dummy_file_name)
     git_handler.return_to_head()
+    git_handler.pop_stash_if_needed()
     assert not exists
 
 
 def test_git_handler_doesnt_delete_non_committed_changes_when_returning(dummy_file_name):
     # Return to a known previous commit
     git_handler = GitHandler('HEAD~5')
+    git_handler.stash_changes_if_necessary()
     git_handler.checkout_first_commit()
     # Come back and assert that those dummy file is still there
     git_handler.return_to_head()
+    git_handler.pop_stash_if_needed()
     assert os.path.exists(dummy_file_name)
